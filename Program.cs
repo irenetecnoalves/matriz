07f36f67792e7687e5daba7b4e5b8c467d38bb54
irenetecnoalves <irenetecnosenior@gmail.com>
2021-07-27 19:59:26 -0300

    exercício de matriz

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n= int.Parse(Console.ReadLine()); 
+           int[,] mat = new int [n, n]; 
+
+           for( int i=0; i<n; i++){
+                 string[] valorlinha= Console.ReadLine().Split(' ');
+
+               for (int col =0; col<n; col++){
+                   mat[i,col] = int.Parse(valorlinha[col]);
+
+               }
+
+           }
+           Console.WriteLine("Diagonal principal");
+           for(int i=0; i<n;i++){
+               Console.Write(mat[i,i]+ "");
+           }
+           Console.WriteLine();
+       
+        int count=0;
+       for (int i=0; i<n; i++){
+           for (int col =0; col<n; col++){
+               if( mat[i,col] <0) {
+                   count++;
+               }
+
+           }
+
+        }
+     Console.WriteLine("A quantidade de numeros negativos são  " +count);
+
+    }
+}
+}
